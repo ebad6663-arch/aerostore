851b4d859a50d6d7d4742543ca38d449416c6d85
Ebad Khan <ebad6663@gmail.com>
2026-08-13 04:09:23 +0500

    fix prisma supabase pooler connection

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClient;
+  prisma: PrismaClient;
 };
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL + "?connection_limit=5",
-      },
-    },
+    datasourceUrl: process.env.DATABASE_URL,
   });
 
 if (process.env.NODE_ENV !== "production") {
