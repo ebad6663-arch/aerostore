b255dfcff0b065be640ffa8535ce8468ac3dece2
Ebad Khan <ebad6663@gmail.com>
2026-08-13 03:33:18 +0500

    fix prisma database connection limit

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as {
+const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient;
 };
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient();
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL + "?connection_limit=5",
+      },
+    },
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
